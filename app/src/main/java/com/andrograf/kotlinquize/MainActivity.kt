93fea18d3e4163cbf9cfcaba5d26a91e24c6a697
Patricia P <andrograf31@gmail.com>
2023-06-26 09:22:52 +0200

    feat: add button to content

--- app/src/main/java/com/andrograf/kotlinquize/MainActivity.kt
+++ app/src/main/java/com/andrograf/kotlinquize/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -48,6 +49,9 @@ fun Quiz(name: String, modifier: Modifier = Modifier) {
             text = "Hello $name!",
             modifier = modifier
         )
+        Button(onClick = { /*TODO*/ }) {
+            "Start"
+        }
     }
 }
 
