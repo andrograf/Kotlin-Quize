13c4116625e8d9b4878c20435c316f6ccf0061fa
Patricia P <andrograf31@gmail.com>
2023-06-26 09:14:48 +0200

    feat: align content to center

--- app/src/main/java/com/andrograf/kotlinquize/MainActivity.kt
+++ app/src/main/java/com/andrograf/kotlinquize/MainActivity.kt
@@ -3,13 +3,18 @@ package com.andrograf.kotlinquize
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.andrograf.kotlinquize.ui.theme.KotlinQuizeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +36,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Quiz(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier){
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(
