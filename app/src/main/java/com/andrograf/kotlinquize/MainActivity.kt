edc968881fd60775690477ef259e32f3e61cbc82
Patricia P <andrograf31@gmail.com>
2023-06-26 10:01:38 +0200

    refactor: extract string

--- app/src/main/java/com/andrograf/kotlinquize/MainActivity.kt
+++ app/src/main/java/com/andrograf/kotlinquize/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,7 +51,7 @@ fun MainPage(modifier: Modifier){
         verticalArrangement = Arrangement.Center,
         modifier = modifier.padding(55.dp)){
         Text(
-            text = "Kotlin Interview Questions",
+            text = stringResource(R.string.title),
             modifier = modifier.padding(20.dp),
             fontSize = 28.sp,
             textAlign = TextAlign.Center
